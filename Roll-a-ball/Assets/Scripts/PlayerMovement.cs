6c483fa4fe831dadc5f38e35c709178d7067638a
xggarcia <guillem.garcia09@estudiant.upf.edu>
2025-04-06 14:13:37 +0200

    BaseGameFinish

--- Roll-a-ball/Assets/Scripts/PlayerMovement.cs
+++ Roll-a-ball/Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro; 
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winText; 
 
     private Rigidbody rb;
-
+    private int points; 
     private float movementX;
     private float movementY;
 
@@ -16,6 +19,21 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        points = 0; 
+        SetCountText();
+        winText.SetActive(false); 
+    }
+
+    void SetCountText()
+    {
+
+        countText.text = "Count: " + points.ToString();
+        if (points >= 12)
+        {
+            winText.SetActive(true);
+
+        }
+
     }
 
     private void OnMove(InputValue movementValue)
@@ -38,6 +56,8 @@ public class PlayerMovement : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            points++;
+            SetCountText();
         }
 
     }
